a186ff2942541feb84418f7e12eb8d80097c38b9
runup <645112281@qq.com>
2013-07-18 12:14:15 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sudo::Application.config.secret_token = '4705f074affca9b33a4d6a7026880842eed1e1de057dd31c0edf60d87fd4ac6acb021270fe7acd401cfdee17ef6ae74fca3a4d65c1e5831eda375b89712e8bf5'
